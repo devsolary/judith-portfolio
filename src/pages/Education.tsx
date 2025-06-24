c7319f3623858a4d691382bc19bb08ef686e245f
devSolary <mathewsolomon91@gmail.com>
2025-06-24 01:18:19 -0700

    add experirnce page

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -2,8 +2,29 @@
 
 const Education = () => {
   return (
-    <div className="h-screen bg-about-bg">
-      
+    <div className="h-screen bg-about-bg pt-32 px-2">
+      <div>
+        <div className="text-white">
+          <h1 className="font-extrabold border-b-8 border-blue-400 w-[30vw] pb-2">EDUCATION</h1>
+          <p> Currently studying Computer Engineering at Lagos
+ State University, I developed strong organizational and
+ data management skills as Assistant Head Girl in high
+ school and Vice President of the Jet Club. I also earned
+ certifications in ICT and Python programming, building
+ a solid foundation in technology. With a focus on
+ accuracy and data integrity, I’m confident in my ability
+ to deliver exceptional results</p>
+        </div>
+        <div className="text-white mt-6">
+          <h1 className="text-4xl font-extrabold">2024-TILL DATE</h1>
+          <div className="mt-5">
+            <h2 className="font-bold">Jubilee model senior grammer school</h2>
+            <p>Science | 2022</p>
+          </div>
+          <div className="font-bold">Bachelor Degree</div>
+          <p>Lagos state university | Bachelor | Till date</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -1,8 +1,46 @@
-
 const Experiences = () => {
   return (
-    <div>Experiences</div>
-  )
-}
+    <div className="h-screen bg-about-bg pt-24">
+      <div className="flex  text-white justify-center content-center pb-12">
+        <h1 className="font-extrabold text-xl mr-12 border-b-4 border-blue-500">
+          EXPERINECE{" "}
+        </h1>
+        <h1 className="text-4xl font-extrabold">2022 - 2023</h1>
+      </div>
+      <div>
+        <div className="text-white">
+          <div>
+            <p className="font-bold text-xl">Luc-thad group of companies</p>
+            <p>Data analyst 2022-2024</p>
+          </div>
+          <div>
+            <p className="py-4">
+              Analyzed and interpreted complex datasets to provide actionable
+              insights, optimized data management processes, and supported
+              strategic decision making. Leveraged tools and techniques to
+              streamline reporting, ensuring accuracy and efficiency in
+              delivering business solutions.
+            </p>
+          </div>
+        </div>
+        <div className="text-white">
+          <div>
+            <p className="font-bold text-xl">Upwork</p>
+            <p>online-freelancer 2023-2024</p>
+          </div>
+          <div>
+            <p className="pt-4">
+              Successfully completed diverse projects in data entry, Python
+              backend development, 3D modeling, and animations. Delivered
+              high-quality solutions tailored to client needs, consistently
+              meeting deadlines and maintaining effective communication to
+              ensure client satisfaction
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Experiences
+export default Experiences;
